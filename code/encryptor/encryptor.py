68656070d81e756654bc6e78ae97e94256b347d4
cabanuel <cabanuel@nps.edu>
2017-05-03 01:50:00 -0700

    added encryption early status

---
+++ code/encryptor/encryptor.py
@@ -0,0 +1,68 @@
+import struct
+
+
+string = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()\x00.,'
+padlong = 111111111111111111111111111111111111111111111111111111111111111111111111111111
+
+message = list(string)
+pad = list(str(padlong))
+
+# message = ['}','B','B','B','B','B','B','B','B','B']
+# pad = ['}','1','1','1','1','1','1','1','1','1']
+encryptedMessage = []
+decryptedMessage = []
+# encryptedMessageRcvd = []
+
+for m,p in zip(message,pad):
+	cipher = (ord(m)^ord(p))
+	encryptedMessage.append(cipher)
+print(encryptedMessage)
+
+messageLen = len(encryptedMessage)
+
+packedencryptedMessage = struct.pack("{0:d}B".format(messageLen), *encryptedMessage)
+
+print(packedencryptedMessage)
+print('\n')
+
+unpackedencryptedMessage = struct.unpack("{0:d}B".format(messageLen), packedencryptedMessage)
+
+print(unpackedencryptedMessage)
+
+
+if (list(unpackedencryptedMessage) == encryptedMessage):
+	print('it works')
+
+else:
+	print('does not work')
+
+
+f = open('cipher.txt','wb')
+# encryptedMessage = bytes(encryptedMessage)
+f.write(packedencryptedMessage)
+f.close
+
+# f = open('cipher.txt','rb')
+# encryptedMessageRcvd = f.read()
+# print(encryptedMessageRcvd)
+
+
+
+# while byte !='\x00':
+# 	byteRead = ord(byte)
+# 	encryptedMessageRcvd.append(byteRead)
+# 	byte = f.read(1)
+
+ 
+
+# for e,p in zip(encryptedMessageRcvd,pad):
+# 	# e = binascii.b2a_base64(e)
+# 	plaintext = chr(ord(e)^ord(p))
+# 	decryptedMessage.append(plaintext)
+# print(decryptedMessage)
+
+
+
+# >>> struct.unpack("{0:d}i".format(numelements),buf)
+
+# >>> struct.pack("{0:d}i".format(numelements), *data)
